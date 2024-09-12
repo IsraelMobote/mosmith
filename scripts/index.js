6a93c5e621b000555faa698d1121bef6e3be952f
IsraelMobote <moboteisrael02@gmail.com>
2024-09-12 17:44:07 +0100

    edit

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 const menuBars = document.querySelector('#menuBars');
 const menuItems = document.querySelector('#menuItems');
+
 menuBars.addEventListener('click', function() {
+    menuBars.classList.toggle('show');
     menuItems.classList.toggle('show');
 })
