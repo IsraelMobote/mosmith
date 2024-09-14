5d5811cae1618bc1315c37b16a5b99b660763236
IsraelMobote <moboteisrael02@gmail.com>
2024-09-14 23:56:41 +0100

    edit

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,14 @@ menuBars.addEventListener('click', function() {
     menuBars.classList.toggle('show');
     menuItems.classList.toggle('show');
 })
+
+const slideText = document.querySelector('.slidetext');
+const sentences = ['We dey for you','We go run am asap','We are one text away'];
+let currentSentence = 0;
+
+function slide() {
+    slideText.textContent = sentences[currentSentence];
+    currentSentence = (currentSentence + 1) % sentences.length;
+}
+
+setInterval(slide, 7000)
